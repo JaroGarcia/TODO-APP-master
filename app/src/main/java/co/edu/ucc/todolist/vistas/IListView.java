a2df9aae77e574833760e2f83644af6e408996c1
JaroGarcia <jarogarcia86@gmail.com>
2017-10-10 11:41:51 -0500

    Initial commit

---
+++ app/src/main/java/co/edu/ucc/todolist/vistas/IListView.java
@@ -0,0 +1,15 @@
+package co.edu.ucc.todolist.vistas;
+
+/**
+ * Created by jggomez on 12-Sep-17.
+ */
+
+public interface IListView {
+
+    void clickAddTarea();
+
+    void refrescarListaTareas();
+
+    void refrescarTarea(int posicion);
+
+}
